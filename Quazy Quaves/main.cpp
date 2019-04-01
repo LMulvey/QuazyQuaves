5b28f7c592b1b43c69a946d54f6398740278857a
Lee Mulvey <lmulvey@me.com>
2019-03-31 22:18:44 -0600

    Initial Commit

---
+++ Quazy Quaves/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Quazy Quaves
+//
+//  Created by Lee Mulvey on 2019-03-31.
+//  Copyright © 2019 Lee Mulvey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
