edfb41905d2cb403951a0914c798a146db8a0be3
Lee Mulvey <lmulvey@me.com>
2019-04-03 22:26:43 -0600

    [master] - Add renderer functions to Graphics class. Begin implementation of sprite class.

--- Quazy Quaves/source/headers/graphics.h
+++ Quazy Quaves/source/headers/graphics.h
@@ -6,8 +6,16 @@
 //  Copyright © 2019 Lee Mulvey. All rights reserved.
 //
 
+/**
+ * Graphics class
+ * Holds all information relevant to rendering
+ * */
+
 #ifndef graphics_h
 #define graphics_h
+
+#include <map>
+#include <string>
 
 struct SDL_Window;
 struct SDL_Renderer;
@@ -16,9 +24,43 @@ class Graphics {
 public:
     Graphics();
     ~Graphics();
+    
+    /**
+     * SDL_Surface* loadImage
+     * Loads an image into the _spriteSheets map if it doesn't already exist.
+     * Each image will only be loaded once.
+     * Returns the image from the map regardless of whether or not it was just loaded
+     * */
+    SDL_Surface* loadImage(const std::string &filePath);
+    
+    /**
+     * void blitSurface
+     * Draws a texture to a certain part of the screen - whatever destination rectangle is
+     * */
+    void blitSurface(SDL_Texture* texture, SDL_Rect* sourceRectangle, SDL_Rect* destinationRectangle);
+    
+    /**
+     * void flip
+     * Renders everything to the screen
+     * */
+    void flip();
+    
+    /**
+     * void clear()
+     * Clears the screen
+     * */
+    void clear();
+    
+    /**
+     * SDL_Renderer* getRenderer
+     * Returns the renderer
+     * */
+    SDL_Renderer* getRenderer() const;
 private:
     SDL_Window* _window;
     SDL_Renderer* _renderer;
+    
+    std::map<std::string, SDL_Surface*> _spriteSheets;
 };
 
 #endif /* graphics_h */
